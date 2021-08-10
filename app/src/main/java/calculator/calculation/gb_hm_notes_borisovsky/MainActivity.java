fa81733a78b9142a215193f773ebc5a51e8ea198
stannavi <stanlevit@yandex.ru>
2021-08-10 17:36:05 +0300

    Initial commit

---
+++ app/src/main/java/calculator/calculation/gb_hm_notes_borisovsky/MainActivity.java
@@ -0,0 +1,14 @@
+package calculator.calculation.gb_hm_notes_borisovsky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
