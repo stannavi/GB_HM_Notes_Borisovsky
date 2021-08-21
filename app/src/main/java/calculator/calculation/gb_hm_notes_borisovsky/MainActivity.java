65502ec472ee345810bb70be7420cfa6d7792bb5
stannavi <stanlevit@yandex.ru>
2021-08-21 09:13:32 +0300

    initial commit

--- app/src/main/java/calculator/calculation/gb_hm_notes_borisovsky/MainActivity.java
+++ app/src/main/java/calculator/calculation/gb_hm_notes_borisovsky/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // черновик +2
+        // черновик +3
     }
 }
